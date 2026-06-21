50298d49ef2355982198f8a027731388ee5c877c
Ilia Ross <ilia@ross.gdn>
2026-06-21 08:06:22 +0200

    Add Clipboard Item Kind support

--- Sources/PasteminApp/ClipboardModels.swift
+++ Sources/PasteminApp/ClipboardModels.swift
@@ -3,3 +3,8 @@ import Carbon
 import Foundation
 
 private let clipboardTextPreviewByteLimit = 1_024
+
+enum ClipboardItemKind: String, Codable, Hashable, Sendable {
+    case text
+    case image
+}
